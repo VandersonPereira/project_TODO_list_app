f9771239ff82ec306a201d23d056aae4087f739d
Vanderson Pereira <vandoap@hotmail.com>
2018-12-20 22:37:12 -0200

    Todo (tasks) search and todo delete

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -15,16 +15,29 @@ export default class Todo extends Component {
         this.state = {description : '', list: []}
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
     }
 
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+        .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    }
+    
     handleAdd(){
         const description = this.state.description
         axios.post(URL, {description})
-        .then(resp => console.log('Tarefa adicionada'))
+        .then(resp => this.refresh())
     }
 
     handleChange(e){
         this.setState({...this.state, description: e.target.value})
+    }
+
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+        .then(resp => this.refresh())
     }
 
     render(){
@@ -32,7 +45,7 @@ export default class Todo extends Component {
         <div>
            <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
            <TodoForm description={this.state.description} handleAdd={this.handleAdd} handleChange={this.handleChange}/>
-           <TodoList/>
+           <TodoList list={this.state.list} handleRemove={this.handleRemove}/>
         </div>
         )
     }
